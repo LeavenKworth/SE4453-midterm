ab6cf6f46a169d5007338a20bca1b18f71ed7f98
DESKTOP-UM8DMA3\ASUS <leavenkworth@gmail.com>
2025-11-18 13:48:55 +0300

    key vault added

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import psycopg2
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+import os
+
+app = Flask(__name__)
+
+# --- Get Key Vault URI from environment variable ---
+KV_URI = os.environ.get("KEY_VAULT_URI")
+
+
+# --- Initialize Key Vault client ---
+credential = DefaultAzureCredential()
+secret_client = SecretClient(vault_url=KV_URI, credential=credential)
+
+# --- Retrieve secrets from Key Vault ---
+DB_USER = secret_client.get_secret("USERNAME").value
+DB_PASSWORD = secret_client.get_secret("PASSWORD").value
+DB_HOST = secret_client.get_secret("HOST").value
+DB_PORT = secret_client.get_secret("PORT").value
+DB_NAME = secret_client.get_secret("DBNAME").value
